91c1862a9773abf1d4ed72810d875a83354ab0c5
4ycg4cxx7m-dotcom <4ycg4cxx7m@privaterelay.appleid.com>
2026-03-05 23:18:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import os
 
 app = Flask(__name__)
 
-# Исправленные маршруты (убрал .html из пути)
-@app.route('/')
-def dalshe():
-    return render_template('index.html')
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
+@app.route('/')
+def qwerd():
+    return render_template('qwerd.html')  # ✅ ПРОСТОЕ ИНТРО - БЕЗ regions!
+
+@app.route('/domoi')
+def domoi():
+    return render_template('index.html')
 
 @app.route('/support_author')
 def support_author():
@@ -25,7 +25,7 @@ def Политика_Конфиденциальности():
 def all_schools():
     return render_template('all_schools.html')
 
-@app.route('/1sch_nskob')
+@app.route('/sch_nskob')
 def Asch_nskob():
     return render_template('Asch_nskob.html')
 
@@ -95,9 +95,6 @@ def sch_nskob_nsk_dzer_sch2():
 def sch_nskob_nsk_dzer_sch1():
     return render_template('Asch_nskob_nsk_dzer_sch1.html')
 
-@app.route('/domoi')
-def domoi():
-    return render_template('home.html')
 
 @app.route('/sch_nskob')
 def sch_nskob():
@@ -107,11 +104,14 @@ def sch_nskob():
 # Списки файлов поиска (ИСПРАВЛЕНО)
 search_all_files = [
     'dalshe.html', 'home.html', 'support_author.html', 'Политика_Конфиденциальности.html',
-    'all_schools.html', 'Информация_о_сайте.html', 'support.html'
+    'all_schools.html', 'Информация_о_сайте.html', 'support.html',     'Asch_nskob_nsk_cent.html', 
+    'Asch_nskob_nsk_cent_gim1.html', 'Asch_nskob_nsk_cent_lic22.html', 
+    'Asch_nskob_nsk_cent_gim13.html','Asch_nskob_nsk_cent_licEkl.html', 'Asch_nskob_nsk_cent_lic12.html',
+    'Asch_nskob_nsk_cent_sch29.html', 'Asch_nskob_nsk_cent_sch54.html',
+    'Asch_nskob_nsk_cent_sch4.html', 'Asch_nskob_nsk_cent_sch156.html',  
 ]
 
 search_nsk_files = [
-    'Asch_nskob_nsk_cent.html', 
     'Asch_nskob_nsk_cent_gim1.html', 'Asch_nskob_nsk_cent_lic22.html', 
     'Asch_nskob_nsk_cent_gim13.html','Asch_nskob_nsk_cent_licEkl.html', 'Asch_nskob_nsk_cent_lic12.html',
     'Asch_nskob_nsk_cent_sch29.html', 'Asch_nskob_nsk_cent_sch54.html',
@@ -124,13 +124,14 @@ search_nsk_files = [
 def get_search_result_info(filename):
     mapping = {
         # Общие страницы
-        'home.html': ('Главная', url_for('home')),
+        'home.html': ('Главная', url_for('domoi')),
         'support_author.html': ('Поддержка автора', url_for('support_author')),
         'Политика_Конфиденциальности.html': ('Политика конфиденциальности', url_for('Политика_Конфиденциальности')),
         'all_schools.html': ('Все школы', url_for('all_schools')),
         'Информация_о_сайте.html': ('Информация о сайте', url_for('Информация_о_сайте')),
         'support.html': ('Поддержка', url_for('support')),
         ### ШКОЛЫ НОВОСИБИРСКА
+        'Asch_nskob_nsk_cent.html': ('Центральный район Новосибирска', url_for('sch_nskob_nsk_cent')),
         'Asch_nskob_nsk_cent_gim1.html': ('Гимназия №1', url_for('gimn1nsk')),
         'Asch_nskob_nsk_cent_lic22.html': ('Лицей №22 «Надежда Сибири»', url_for('nslic220nsk')),
         'Asch_nskob_nsk_cent_gim13.html': ('Гимназия №13 им. Э.А. Быкова', url_for('Asch_nskob_nsk_cent_gim13')),
@@ -183,4 +184,4 @@ def search():
     return render_template('search.html')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8800, ssl_context='adhoc', debug=True)
+    app.run(host='127.0.0.1', port=5000, ssl_context='adhoc', debug=True)
